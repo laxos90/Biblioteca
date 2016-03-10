945911df7761e564d2d36125c01cc876d69e8a18
m <lmiguelvargasf@gmail.com>
2016-03-10 17:31:33 -0500

    Display the list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to The Bangalore Public Library");
+        ArrayList<String> books = new ArrayList<String>() {{
+            add("A Thousand Splendid Suns");
+            add("Hunger Games");
+            add("Two Towers");
+        }};
+
+        for (String book: books) {
+            System.out.println(book);
+        }
     }
 }
