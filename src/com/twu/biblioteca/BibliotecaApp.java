7ccb62bc27c993bef56eafbc36e40aa917d7b9a6
m <lmiguelvargasf@gmail.com>
2016-03-11 12:04:02 -0500

    Extract method printBooks() and remove import statement

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,15 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to The Bangalore Public Library");
         Library library = new Library();
 
+        printBooks(library);
+    }
+
+    private static void printBooks(Library library) {
         for (Book book: library.getBooks()) {
             System.out.println(book);
         }
