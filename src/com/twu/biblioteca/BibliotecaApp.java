bdebada03a3d7214383edd5e01a1148f246b6f03
m <lmiguelvargasf@gmail.com>
2016-03-11 12:27:56 -0500

    Allow user to choose option of listing books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,22 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to The Bangalore Public Library");
         Library library = new Library();
 
-        printBooks(library);
+        System.out.println("Choose an option");
+        System.out.println("1 - List available books");
+
+        Scanner reader = new Scanner(System.in);
+        int option = reader.nextInt();
+
+        if (option == 1) {
+            printBooks(library);
+        }
     }
 
     private static void printBooks(Library library) {
