927556c9934d34f321dbe0234b91aee4decc654a
m <lmiguelvargasf@gmail.com>
2016-03-11 12:31:44 -0500

    Extract method printWelcomeMessageAndMenuOptions

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to The Bangalore Public Library");
         Library library = new Library();
-
-        System.out.println("Choose an option");
-        System.out.println("1 - List available books");
+        printWelcomeMessageAndMenuOptions();
 
         Scanner reader = new Scanner(System.in);
         int option = reader.nextInt();
@@ -17,6 +14,12 @@ public class BibliotecaApp {
         if (option == 1) {
             printBooks(library);
         }
+    }
+
+    private static void printWelcomeMessageAndMenuOptions() {
+        System.out.println("Welcome to The Bangalore Public Library");
+        System.out.println("Choose an option");
+        System.out.println("1 - List available books");
     }
 
     private static void printBooks(Library library) {
