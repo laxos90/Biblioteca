3473c5e7d65d8482a7c45048b9849ca84c051803
d <laxos90@gmail.com>
2016-03-14 16:07:55 -0500

    Create Reader class and move methods from BibliotecaApp

---
+++ src/com/twu/biblioteca/Reader.java
@@ -0,0 +1,18 @@
+package com.twu.biblioteca;
+
+import java.util.Scanner;
+
+/**
+ * Created by user on 3/14/16.
+ */
+public class Reader {
+    public static int getUserOption() {
+        Scanner reader = new Scanner(System.in);
+        return reader.nextInt();
+    }
+
+    public static String getBookTitleFromUser() {
+        Scanner reader = new Scanner(System.in);
+        return reader.nextLine();
+    }
+}
